762c58347ec6f28f144c925aa228b9784462e57a
CyberImpXIII <jnelsenepstein@gmail.com>
2022-05-18 01:38:10 -0400

    initial event schema and api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+class Event {
+  name: string;
+  constructor(theName:string){
+    this.name = theName;
+  }
+}
 function App() {
+  const [events, setEvents] = useState<Array<Event>>([{name:''}])
+  useEffect(()=>{
+    axios(
+      {
+        url:"https://app.ticketmaster.com/discovery/v2/events",
+        params:{
+          "apikey": "Mx0hOk19jA4oOAyy5kphNjMopcL2xvUf"
+        }
+      })
+  .then(data=>{
+    console.log(data.data._embedded.events)
+    setEvents(data.data._embedded.events)
+  })
+  },[])
+  // useEffect(()=>{
+  //   console.log(events)
+  // },[events])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        test
       </header>
+      <div>
+      {events.map((event)=>(<div>{event.name}</div>))}
+      </div>
     </div>
+
   );
 }
 
